80b34c46e9d8742e7fad08fe8f219fb2501f39b6
yuukilion <hy130310@gmail.com>
2023-01-13 21:29:21 +0900

    [add]ヘッダー、各種ページへのルート処理を備えたひな形を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout } from 'antd';
+import { GoogleCircleFilled, HeartFilled, ShoppingCartOutlined } from '@ant-design/icons';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { IndexPage } from './pages/IndexPage';
+import { DetailPage } from './pages/DetailPage';
+import { WishListPage } from './pages/WishListPage';
+import { CartPage } from './pages/CartPage';
+
+
+const { Header } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header style={{position: 'sticky', top: 0, zIndex: 1, width: '100%', backgroundColor: 'gray'}}>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+          <div>
+            <GoogleCircleFilled style={{color: 'white', fontSize: '24px'}}/>
+          </div>
+          <div>
+            <HeartFilled style={{color: 'white', fontSize: '24px'}}/>
+            <ShoppingCartOutlined style={{color: 'white', fontSize: '24px'}}/>
+          </div>
+        </div>
+      </Header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<IndexPage/>}/>
+            <Route path='/detail' element={<DetailPage/>}/>
+            <Route path='/wishlist' element={<WishListPage/>}/>
+            <Route path='/cart' element={<CartPage/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Layout>
   );
 }
 
