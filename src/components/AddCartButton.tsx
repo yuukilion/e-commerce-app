925928fd3e15327cf913bb17363f08c36cee4acb
yuukilion <hy130310@gmail.com>
2023-01-21 13:39:20 +0900

    [add]修正途中のファイルを追加

---
+++ src/components/AddCartButton.tsx
@@ -0,0 +1,11 @@
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+
+export const AddCartButton = (props: any) => {
+    const { switchItemInCart } = props
+    return (
+        <Button shape='circle' onClick={switchItemInCart}>
+            <ShoppingCartOutlined/>
+        </Button>
+    );
+};
--- src/components/MyHeartButton.tsx
+++ src/components/MyHeartButton.tsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import { HeartFilled } from '@ant-design/icons';
+import { Button } from "antd";
 
-export const MyHeartButton = () => {
+export const MyHeartButton = (props: any) => {
+  const { toggleWishList } = props;
   const [isClicked, setIsClicked] = useState(false);
 
+  const addWishList = () => {
+    setIsClicked(prev => !prev);
+    toggleWishList();
+  }
   return (
-    <>
-      <HeartFilled style={{color: isClicked ? 'red': ''}} onClick={()=> setIsClicked(prev => !prev)}/>
-    </>
+    <Button shape="circle" danger={isClicked} style={{}} onClick={addWishList}>
+      <HeartFilled style={{color: isClicked ? 'red': ''}} />
+    </Button>
   );
 };
