a1cb7cae6db3cc31ef6840a12aec4840a8456967
yuukilion <hy130310@gmail.com>
2023-01-21 13:35:55 +0900

    [add]定数を管理するファイルを追加

---
+++ src/state/constant.ts
@@ -0,0 +1,48 @@
+export const CATEGORY_TITLE_LIST = ['SmartPhone', 'SmartWatch'];
+
+export const PURCHASE_QUANTITY_OPTIONS = [
+    {
+      value: 1,
+      label: '1'
+    },
+    {
+      value: 2,
+      label: '2'
+    },
+    {
+      value: 3,
+      label: '3'
+    },
+    {
+      value: 4,
+      label: '4',
+    },
+    {
+      value: 5,
+      label: '5'
+    }
+];
+
+export const SORT_OPTIONS = [
+    {
+      value: 0,
+      label: '高'
+    },
+    {
+      value: 1,
+      label: '低'
+    }
+];
+
+export const FILTERING_OPTIONS = [
+{
+    value: 0,
+    label: 'スマートフォン'
+},
+{
+    value: 1,
+    label: 'スマートウォッチ'
+}
+];
+
+export const DESCENDING_NUM = 0;
